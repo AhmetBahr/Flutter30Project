de912d279f8bd6161b5b094e588ed0cb3e8d20ab
Ahmet.B <ahmetbhr29@gmail.com>
2023-03-29 13:25:14 +0300

    Show Website Finish

--- ShowWebsite/show_website/lib/main.dart
+++ ShowWebsite/show_website/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/material/theme_data.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +15,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
+
+  final _controller = Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,14 +25,69 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Avsa.H\'s Website"),
         ),
+        body: WebView(
+          initialUrl: "https://github.com/AhmetBahr",
+          onWebViewCreated: (controller) => _controller.complete(controller),
+        ),
+
         bottomNavigationBar: Container(
-          color: Theme.of(context).accentColor,
-          child: ButtonBar(),
+          color: Colors.red.shade400,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20, right:  20), 
+            child: ButtonBar(
+              children: [
+                navigationButton(Icons.chevron_left, (controller) => _goBack(controller)),
+                navigationButton(Icons.chevron_right, (controller) => _goForward(controller)),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 
 
-  
+  Widget navigationButton(IconData icon,Function(WebViewController) onPressed){
+    return FutureBuilder(
+      future: _controller.future,
+      builder: (context, AsyncSnapshot<WebViewController> snapshot){
+        if(snapshot.hasData){
+
+        return IconButton(
+        onPressed: () => onPressed(snapshot.data),
+       icon: Icon(
+        icon,
+        color:Colors.white
+        ),
+       );
+      }
+      else{
+        return Container();
+      }
+    }
+  );
 }
+
+
+
+    void _goBack(WebViewController controller) async {
+    final canGoBack = await controller.canGoBack();
+
+    if (canGoBack) {
+      controller.goBack();
+    }
+  }
+
+  void _goForward(WebViewController controller) async {
+    final canGoForward = await controller.canGoForward();
+
+    if (canGoForward) {
+      controller.goForward();
+    }
+  }
+
+}
+
+
+
+
