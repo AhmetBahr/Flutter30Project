cdc8eb3223baf9355098bec0fceb0b21b265d4b7
Ahmet.B <ahmetbhr29@gmail.com>
2023-03-29 13:01:39 +0300

    Show Website Fixed Bugs

--- ShowWebsite/show_website/lib/main.dart
+++ ShowWebsite/show_website/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/material/theme_data.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,15 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold()
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Avsa.H\'s Website"),
+        ),
+        bottomNavigationBar: Container(
+          color: Theme.of(context).accentColor,
+          child: ButtonBar(),
+        ),
+      ),
     );
   }
 
