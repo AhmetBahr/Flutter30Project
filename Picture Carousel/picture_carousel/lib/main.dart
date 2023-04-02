d73a48e0ad1a4626ab65b0626e06757a9538247c
Ahmet.B <ahmetbhr29@gmail.com>
2023-04-02 14:09:47 +0300

    Picture Carousel / Finish

--- Picture Carousel/picture_carousel/lib/main.dart
+++ Picture Carousel/picture_carousel/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +11,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp>{
+
+  final _imagePaths = [
+    'images/1.png',
+    'images/2.png',
+    'images/3.png',
+    'images/4.png',
+    'images/5.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
-
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: CarouselSlider(
+            items: _imagePaths.map((imagePath) {
+              return Builder(builder: (context){
+                return Container(
+                  color: Colors.redAccent,
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  child: Image.asset(imagePath)
+                  );
+              });
+            }).toList(),
+            options: CarouselOptions(
+              autoPlay: true,
+            ),
+          ),
+        ),
+        
+      )
+    );
   }
-  
 }
