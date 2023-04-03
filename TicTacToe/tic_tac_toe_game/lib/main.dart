9fff4614a9bc3f4f64b723685270b02d25809a6e
Ahmet.B <ahmetbhr29@gmail.com>
2023-04-03 19:00:19 +0300

    Tic Tac Toe / Board

--- TicTacToe/tic_tac_toe_game/lib/main.dart
+++ TicTacToe/tic_tac_toe_game/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_game/tile_state.dart';
+
+import 'board_tile.dart';
 
 void main() {
   runApp( MyApp());
@@ -10,10 +13,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp>{
+
+  var _boardState = List.filled(9, TileState);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: Stack(children: [Image.asset("images/board.png"), _borderTiles()]),
+        ),
+      )
+    );
 
   }
   
+Widget _borderTiles(){
+  return Builder(builder: (context){
+    final boardDismension = MediaQuery.of(context).size.width;
+    final tileDimension = boardDismension / 3; 
+
+    return Container(
+      width:  boardDismension,
+      height: boardDismension,
+     // color: Colors.redAccent,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              BoardTile(dimension: tileDimension,),
+              BoardTile(dimension: tileDimension,),
+              BoardTile(dimension: tileDimension,),
+            ],
+          ),
+          Row(
+            children: [
+              BoardTile(dimension: tileDimension,),
+              BoardTile(dimension: tileDimension,),
+              BoardTile(dimension: tileDimension,),
+            ],
+          ),
+          Row(
+            children: [
+              BoardTile(dimension: tileDimension,),
+              BoardTile(dimension: tileDimension,),
+              BoardTile(dimension: tileDimension,),
+            ],
+          ),
+        ],
+      ) ,
+
+    );
+
+
+  });
+}
+
 }
