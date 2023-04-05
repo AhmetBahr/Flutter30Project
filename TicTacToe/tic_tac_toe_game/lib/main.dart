e7df17f9d1c57d74080dc11e00c6bf64bbccb117
Ahmet.B <ahmetbhr29@gmail.com>
2023-04-05 17:56:26 +0300

    That is GAME!!

--- TicTacToe/tic_tac_toe_game/lib/main.dart
+++ TicTacToe/tic_tac_toe_game/lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp>{
 
-  var _boardState = List.filled(9, TileState);
+  var _boardState = List.filled(9, TileState.EMPTY);
+
+  var _currentTurn = TileState.CROSS;
 
   @override
   Widget build(BuildContext context) {
@@ -38,35 +40,74 @@ Widget _borderTiles(){
       height: boardDismension,
      // color: Colors.redAccent,
       child: Column(
-        children: [
-          Row(
-            children: [
-              BoardTile(dimension: tileDimension,),
-              BoardTile(dimension: tileDimension,),
-              BoardTile(dimension: tileDimension,),
-            ],
-          ),
-          Row(
-            children: [
-              BoardTile(dimension: tileDimension,),
-              BoardTile(dimension: tileDimension,),
-              BoardTile(dimension: tileDimension,),
-            ],
-          ),
-          Row(
-            children: [
-              BoardTile(dimension: tileDimension,),
-              BoardTile(dimension: tileDimension,),
-              BoardTile(dimension: tileDimension,),
-            ],
-          ),
-        ],
-      ) ,
+         children: chunk(_boardState, 3).asMap().entries.map((entry) {
+            final chunkIndex = entry.key;
+            final tileStateChunk = entry.value;
 
-    );
+            return Row(
+              children: tileStateChunk.asMap().entries.map((innerEntry) {
+                final innerIndex = innerEntry.key;
+                final tileState = innerEntry.value;
+                final tileIndex = (chunkIndex * 3) + innerIndex;
+
+                return BoardTile(
+                  tileState: tileState,
+                  dimension: tileDimension,
+                // onPressed: () => print("Tapped index $tileIndex"),
+                  onPressed: () => _updateTileStateForIndex(tileIndex),
+                );
+              }).toList(),
+            );
+          }).toList()));
+    });
+  }
+
+  void _updateTileStateForIndex(int selectedIndex){
+    if(_boardState[selectedIndex] == TileState.EMPTY){
+      setState(() {
+        _boardState[selectedIndex] = _currentTurn;
+        _currentTurn =_currentTurn == TileState.CROSS ? TileState.CIRCLE: TileState.CROSS;
+      });
+
+      final winner = _findWinner();
+      if(winner != null)
+        print("Winner is $winner");
+
+    }
+  }
+
+  TileState _findWinner(){
+        TileState Function(int, int, int) winnerForMatch = (a, b, c) {
+      if (_boardState[a] != TileState.EMPTY) {
+        if ((_boardState[a] == _boardState[b]) &&
+            (_boardState[b] == _boardState[c])) {
+          return _boardState[a];
+        }
+      }
+      return null;
+  };
+
+    final checks = [
+        winnerForMatch(0, 1, 2),
+        winnerForMatch(3, 4, 5),
+        winnerForMatch(6, 7, 8),
+        winnerForMatch(0, 3, 6),
+        winnerForMatch(1, 4, 7),
+        winnerForMatch(2, 5, 8),
+        winnerForMatch(0, 4, 8),
+        winnerForMatch(2, 4, 6),
+      ];
+
+      TileState winner;
+      for (int i = 0; i < checks.length; i++) {
+        if (checks[i] != null) {
+          winner = checks[i];
+          break;
+        }
+      }
+    return winner;
+  }
 
 
-  });
-}
 
 }
