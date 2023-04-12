e0e7e87669ab165e33710be0ba732c868645297c
Ahmet.B <ahmetbhr29@gmail.com>
2023-04-12 17:55:18 +0300

    Networking / Finish

--- Networking/networking_request/lib/main.dart
+++ Networking/networking_request/lib/main.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:networking_request/data_service.dart';
+
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState () => _MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
+  final _dataService = DataService();
+  late String _response;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
-
+    return MaterialApp(
+      home: Scaffold(body: Center(
+        child: Builder(builder: (_) {
+          if (_response != null) {
+            return Text(_response);
+          } else {
+            return ElevatedButton(
+              child: Text('Make Request'),
+              onPressed: _makeRequest,
+            );
+          }
+        }),
+      )),
+    );
   }
-  
+
+  void _makeRequest() async {
+    final response = await _dataService.makeRequestToApi();
+    setState(() => _response = response);
+  }
 }
